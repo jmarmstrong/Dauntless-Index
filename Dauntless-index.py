52c2d8db64b7cabe4b201f1dfbea5f0e3ca211db
BlueFlameFox <blueflamefox21@gmail.com>
2019-06-06 17:33:05 +0100

    added loops

--- Dauntless-index.py
+++ Dauntless-index.py
@@ -15,54 +15,84 @@ while loop:
        """,)
     variable = input("Please enter the number of the type of Behemoth ")
     if variable == "1":
-        loop = False
-        pp("""
-    1) Gnasher
-    2) Quillshot
-    3) Shrike
-    """)
-        neutral = input("Please enter the number of the behemoth")
-    
-        if neutral == "1":
+        loop2 = False
+        while loop2:
+            loop = False
             pp("""
-    1) Lesser Gnasher
-    2) Gnasher
-    3) Ragetail Gnasher
-    """)
-            gnasher = input("Please enter the number for the type of Gnasher")
-            if gnasher == "1":
-                g = open("lgnasher.txt", "r") #https://www.guru99.com/reading-and-writing-files-in-python.html
-                if g.mode == "r":
-                    contents = g.read()
-                    print(contents)
-                    yn = input("do you want to return to the beginning Y/N: ")
-                    if yn == "Y" or yn == "y":
-                        loop = True
-                    if yn == "N" or yn == "n":
-                        exit()
+        1) Gnasher
+        2) Quillshot
+        3) Shrike
+        """)
+            neutral = input("Please enter the number of the behemoth")
+    else:
+        loop = True
+        
+        if neutral == "1":
+                while loop3:
+                    pp("""
+            1) Lesser Gnasher
+            2) Gnasher
+            3) Ragetail Gnasher
+            """)
+                    gnasher = input("Please enter the number for the type of Gnasher")
+                    if gnasher == "1":
+                        while loop4:
+                            g = open("lgnasher.txt", "r") #https://www.guru99.com/reading-and-writing-files-in-python.html
+                            if g.mode == "r":
+                                contents = g.read()
+                                print(contents)
+                                yn = input("do you want to return to the beginning Y/N: ")
+                                if yn == "Y" or yn == "y":
+                                    loop = True
+                                else:
+                                    loop4 = True
+                                if yn == "N" or yn == "n":
+                                    exit()
+                                else:
+                                    loop4 = True
+                    else:
+                        loop3 = True
+                                
+                    if gnasher == "2":
+                        while loop5:
+                            g = open("gnasher.txt", "r")
+                            if g.mode == "r":
+                                contents = g.read()
+                                print (contents)
+                                yn = input("do you want to return to the beginning Y/N: ")
+                                if yn == "Y" or yn == "y":
+                                    loop = True
+                                else:
+                                    loop5 = True
+                                if yn == "N" or yn == "n":
+                                    exit()
+                                else:
+                                    loop5 = True
+                    else:
+                        loop3 = True
                         
-            if gnasher == "2":
-                g = open("gnasher.txt", "r")
-                if g.mode == "r":
-                    contents = g.read()
-                    print (contents)
-                    yn = input("do you want to return to the beginning Y/N: ")
-                    if yn == "Y" or yn == "y":
-                        loop = True
-                    if yn == "N" or yn == "n":
-                        exit()
-                        
-            if gnasher == "3":
-                g = open("rgnasher.txt", "r")
-                if g.mode == "r":
-                    contents = g.read()
-                    print (contents)
-                    yn = input("do you want to return to the beginning Y/N: ")
-                    if yn == "Y" or yn == "y":
-                        loop = True
-                    if yn == "N" or yn == "n":
-                        exit()
-        if neutral == "2":
+                    if gnasher == "3":
+                        while loop6:
+                            g = open("rgnasher.txt", "r")
+                            if g.mode == "r":
+                                contents = g.read()
+                                print (contents)
+                                yn = input("do you want to return to the beginning Y/N: ")
+                                if yn == "Y" or yn == "y":
+                                    loop = True
+                                else:
+                                    loop6 = True
+                                if yn == "N" or yn == "n":
+                                    exit()
+                                else:
+                                    loop6 = True
+                    else:
+                        loop3 = True
+        else:
+            loop2 = True
+            
+    if neutral == "2":
+        while loop7:
             q = open("quillshot.txt", "r")
             if q.mode == "r":
                     contents = q.read()
@@ -70,26 +100,33 @@ while loop:
                     yn = input("do you want to return to the beginning Y/N: ")
                     if yn == "Y" or yn == "y":
                         loop = True
+                    else:
+                        loop7 = True
                     if yn == "N" or yn == "n":
                         exit()
-                        
-        if neutral == "3":
-            while loop2:
-                loop2 = False
-                q = open("shrike.txt", "r")
-                if q.mode == "r":
-                        contents = q.read()
-                        print (contents)
-                        yn = input("do you want to return to the beginning Y/N: ")
-                        if yn == "Y" or yn == "y":
-                            loop = True
-                        else:
-                            loop2 = True
-                        if yn == "N" or yn == "n":
-                            exit()
-                        else:
-                            loop2 = True
-                        
+                    else:
+                        loop7 = True
+    else:
+        loop2 = True
+                                
+    if neutral == "3":
+        while loop8:
+            loop2 = False
+            q = open("shrike.txt", "r")
+            if q.mode == "r":
+                    contents = q.read()
+                    print (contents)
+                    yn = input("do you want to return to the beginning Y/N: ")
+                    if yn == "Y" or yn == "y":
+                        loop = True
+                    else:
+                        loop8 = True
+                    if yn == "N" or yn == "n":
+                        exit()
+                    else:
+                        loop8 = True
+    else:
+        loop2 = True
                     
     if variable == "Blaze" or variable ==  "blaze":
         loop = False
